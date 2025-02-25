ad077d0b543fd91407d9204aae9390ebba9233ec
miskogepa <milovan.jankovic50m@gmail.com>
2025-02-25 20:16:57 +0100

    druga

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const homeBtn = document.getElementById('home-btn');
     const newMissionBtn = document.getElementById('new-mission-btn');
     const missionBtn = document.getElementById('mission-btn');
+    const addPhaseBtn = document.getElementById('add-phase');
 
     if (homeBtn) {
         homeBtn.addEventListener('click', function() {
@@ -18,6 +19,12 @@ document.addEventListener('DOMContentLoaded', function() {
     if (missionBtn) {
         missionBtn.addEventListener('click', function() {
             window.location.href = 'mission.html';
+        });
+    }
+
+    if (addPhaseBtn) {
+        addPhaseBtn.addEventListener('click', function() {
+            window.location.href = 'newphase.html';
         });
     }
 });
